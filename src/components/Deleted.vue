b473a3b02bd88346aa169aedbfff01f06a19734e
ReihaneENS <ebrahiminsb@gmail.com>
2023-07-15 20:20:52 +0330

    Delete Employee Added

---
+++ src/components/Deleted.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Deleted"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Edited.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Edited"
+}
+</script>
+
+<style scoped>
+
+</style>
